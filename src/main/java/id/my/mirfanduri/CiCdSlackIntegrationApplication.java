07296bf32c10cbfb9dbe0c63ca0a4d738fba297b
sidulanaksekolahan <mirfan.duri@gmail.com>
2024-08-08 17:47:06 +0700

    First commit

---
+++ src/main/java/id/my/mirfanduri/CiCdSlackIntegrationApplication.java
@@ -0,0 +1,13 @@
+package id.my.mirfanduri;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CiCdSlackIntegrationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CiCdSlackIntegrationApplication.class, args);
+	}
+
+}
