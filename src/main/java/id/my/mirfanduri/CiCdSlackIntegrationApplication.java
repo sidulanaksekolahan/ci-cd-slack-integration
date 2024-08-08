46d23c5cab5a40211a1d9e956347466ea58fa283
sidulanaksekolahan <mirfan.duri@gmail.com>
2024-08-08 18:00:56 +0700

    Added simple print

--- src/main/java/id/my/mirfanduri/CiCdSlackIntegrationApplication.java
+++ src/main/java/id/my/mirfanduri/CiCdSlackIntegrationApplication.java
@@ -1,13 +1,22 @@
 package id.my.mirfanduri;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class CiCdSlackIntegrationApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CiCdSlackIntegrationApplication.class, args);
+	}
+
+	@Bean
+	public CommandLineRunner commandLineRunner() {
+		return args -> {
+			System.out.println("Test!");
+		};
 	}
 
 }
